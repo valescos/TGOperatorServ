6c458cbf72e32d40b0523bab4d800988a41a273c
Kosarev Valentin <valescos1986@gmail.com>
2025-01-30 18:22:26 +0700

    Responce added

--- index.js
+++ index.js
@@ -8,26 +8,10 @@ app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
     const update = req.body;
-
     console.log('Received update:', update);
-
-    // if (update.message) {
-    //     const chatId = update.message.chat.id;
-    //     const text = update.message.text;
-
-    //     console.log(`Message from chat ${chatId}: ${text}`);
-
-    //     const response = {
-    //         method: 'sendMessage',
-    //         chat_id: chatId,
-    //         text: `You said: ${text}`
-    //     };
-
-    //     res.json(response);
-    // } else {
-    //     res.sendStatus(200);
-    // }
-});
+    res.sendStatus(200);
+    }
+);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
