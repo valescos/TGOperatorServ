c1ee9da43a45cbe1d8392478ed152da5398ef517
Kosarev Valentin <valescos1986@gmail.com>
2025-01-30 18:45:01 +0700

    JSON stringify

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
     const update = req.body;
-    console.log('message_thread_id', update.message.message_thread_id,
-         'text', update.message.text, "is_bot", update.message.from.is_bot);
+    console.log(JSON.stringify({
+        "message_thread_id": update.message.message_thread_id,
+        "text": update.message.text
+    }));
     res.sendStatus(200);
     }
 );
