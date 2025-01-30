149324c379ae2b0de3523f3ea730365ee0e1e478
Kosarev Valentin <valescos1986@gmail.com>
2025-01-30 18:30:56 +0700

    Fixes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
     const update = req.body;
-    console.log('Received update:', update);
+    console.log('Received update:', update.message.message_thread_id, update.message.text, update.message.reply_to_message.chat);
     res.sendStatus(200);
     }
 );
